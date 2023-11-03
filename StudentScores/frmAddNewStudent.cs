3c91540bbd4d86affa8a421cd7e681a49b45ab2a
B1adeMastr <93338755+B1adeMastr@users.noreply.github.com>
2023-11-02 20:03:27 -0600

    keeping up with the demo, all code green lights

--- StudentScores/frmAddNewStudent.cs
+++ StudentScores/frmAddNewStudent.cs
@@ -29,15 +29,100 @@ namespace StudentScores
                 txtScore.Focus();
             }
         }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            scoreList.Clear();
+            lblScores.Text = "";
+            lblScores.Focus();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (IsValidName())
+            {
+                string studentScores = txtName.Text;
+                foreach (int score in scoreList)
+                {
+                    studentScores += $"{score}";
+                }
+                Tag = studentScores;
+                DialogResult = DialogResult.OK;
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
         private void DisplayScores()
         {
-            StringBuilder sb = new StringBuilder(); 
+            StringBuilder sb = new StringBuilder();
             foreach (int score in scoreList)
             {
                 sb.Append($"{score} ");
             }
             lblScores.Text = sb.ToString();
+        }
+
+        private bool IsValidScore()
+        {
+            bool success = true;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(IsPresent(txtScore.Text, "Score"));
+            sb.Append(IsInt32(txtScore.Text, "Score"));
+            sb.Append(IsWithinRange(txtScore.Text, "Score", 0, 200));
+            string errorMsg = sb.ToString();
+
+            if (!String.IsNullOrEmpty(errorMsg))
+            {
+                success = false;
+                MessageBox.Show(errorMsg, "Entry Error");
+            }
+
+            return success;
+        }
+
+        private bool IsValidName()
+        {
+            bool success = true;
+            string errorMsg = IsPresent(txtName.Text, "Name");
+            if (!String.IsNullOrEmpty(errorMsg))
+            {
+                success = false;
+                MessageBox.Show(errorMsg, "Entry Error");
+            }
+            return success;
+        }
+
+        private string IsPresent(string value, string name)
+        {
+            string errorMsg = "";
+                if (String.IsNullOrEmpty(value)) {
+                errorMsg = $"{name} is a required field. \n";
+            }
+            return errorMsg;
+        }
+        private string IsInt32(string value, string name) {
+            string errorMsg = "";
+            if (!Int32.TryParse(value, out _))
+            {
+                errorMsg = $"{name} is not a valid integer \n";
+            }
+            return errorMsg;
+        }
+
+        private string IsWithinRange(string value, string name, decimal min, decimal max) {
+            string errorMsg = "";
+            if (Decimal.TryParse(value, out decimal number)) {
+                if (number < min || number > max)
+                {
+                    errorMsg = $"{name} must be between {min} and {max}. \n";
+                }
+            }
+            return errorMsg;
         }
     }
 }
